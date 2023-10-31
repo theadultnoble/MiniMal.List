49951c0664439d876b1dcea9288ce7a2d6fd2234
Noble Okafor <okafornoble202@gmail.com>
2023-10-31 13:43:02 +0100

    css styling done

--- src/screens/Welcomescreen.tsx
+++ src/screens/Welcomescreen.tsx
@@ -1,11 +1,12 @@
 import React, { useRef, useCallback, useMemo } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
+import { useNavigation } from "@react-navigation/native";
 
 const Welcomescreen = () => {
   return (
-    <View>
-      <Text style={{ fontSize: 40, fontFamily: "IndieFlower" }}>
+    <View style={styles.container}>
+      <Text style={{ fontSize: 70, fontFamily: "IndieFlower" }}>
         MiniMal-List
       </Text>
       <StatusBar />
@@ -19,6 +20,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "center",
     padding: 20,
   },
 });
