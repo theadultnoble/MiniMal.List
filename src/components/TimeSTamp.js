efc506c3465b751deb19a84cd7c69bd7833c2c10
Noble Okafor <okafornoble202@gmail.com>
2023-11-25 23:46:47 +0100

    create timestamp component

---
+++ src/components/TimeSTamp.js
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const TimeSTamp = () => {
+  return (
+    <View>
+      <Text>9.00</Text>
+    </View>
+  );
+};
+
+export default TimeSTamp;
+
+const styles = StyleSheet.create({
+  textSTamp: {
+    fontFamily: "Poppins_400Regular",
+    fontSize: 14,
+  },
+});
