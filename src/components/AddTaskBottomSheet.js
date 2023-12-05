db895656881835c957e6035421a969b9cca550fc
Noble Okafor <okafornoble202@gmail.com>
2023-12-05 23:26:29 +0100

    few updates

--- src/components/AddTaskBottomSheet.js
+++ src/components/AddTaskBottomSheet.js
@@ -1,11 +1,19 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { React, useMemo, useRef } from "react";
+import BottomSheet from "@gorhom/bottom-sheet";
 
-const AddTaskBottomSheet = () => {
+const AddTaskBottomSheet = ({ innerRef, closeButton }) => {
+  const snapPoints = useMemo(() => ["95%"]);
+
   return (
-    <View>
-      <Text>AddTaskBottomSheet</Text>
-    </View>
+    <>
+      <BottomSheet ref={innerRef} snapPoints={snapPoints} index={0}>
+        <View style={{ flex: 1, alignItems: "center" }}>
+          <Text>Awesome 🎉 Tasklist</Text>
+          <Button onPress={() => closeButton()} title="Close" />
+        </View>
+      </BottomSheet>
+    </>
   );
 };
 
