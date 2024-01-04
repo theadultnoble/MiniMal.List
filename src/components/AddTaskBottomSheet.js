f3b760c2ae3a74e31768ff2563755802d037e77d
Noble Okafor <okafornoble202@gmail.com>
2024-01-04 23:31:06 +0100

    progress bottomsheet design

--- src/components/AddTaskBottomSheet.js
+++ src/components/AddTaskBottomSheet.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, Pressable } from "react-native";
 import { React, useMemo, useRef } from "react";
 import BottomSheet from "@gorhom/bottom-sheet";
 
@@ -8,9 +8,12 @@ const AddTaskBottomSheet = ({ innerRef, closeButton }) => {
   return (
     <>
       <BottomSheet ref={innerRef} snapPoints={snapPoints} index={0}>
-        <View style={{ flex: 1, alignItems: "center" }}>
-          <Text>Awesome 🎉 Tasklist</Text>
-          <Button onPress={() => closeButton()} title="Close" />
+        <View style={styles.container}>
+          <View>
+            <Pressable style={styles.doneButton} onPress={() => closeButton()}>
+              <Text style={{ color: "white", fontSize: 20 }}>Close</Text>
+            </Pressable>
+          </View>
         </View>
       </BottomSheet>
     </>
@@ -19,4 +22,20 @@ const AddTaskBottomSheet = ({ innerRef, closeButton }) => {
 
 export default AddTaskBottomSheet;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    borderColor: "black",
+    borderWidth: "0.5px",
+    padding: 15,
+  },
+  doneButton: {
+    borderWidth: "1px",
+    backgroundColor: "#2A2A2A",
+    borderRadius: "10px",
+    width: 80,
+    height: 40,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
